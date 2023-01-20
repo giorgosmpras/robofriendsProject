874ec3c911e5b8d6ab6b9ae9e303c2a9c0252c1a
giorgosmpras <geobbras@gmail.com>
2023-01-20 12:12:40 +0200

    modifing the json package

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -2,9 +2,6 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-  if (true) {
-    throw new Error("NOOOOOO");
-  }
   const cardArray = robots.map((user, i) => {
     return (
       <Card
